a822dbca8f1ced4af130447a980f0554eafdd0b6
huise0ng <shinhuiseong07@gmail.com>
2024-02-17 17:41:36 +0900

    ADD :: Axios getArticle 라우터

--- board/src/views/HomeView.vue
+++ board/src/views/HomeView.vue
@@ -2,3 +2,18 @@
   <h1>Hello,Huiseong / 안녕, 희성</h1>
 </template>
 
+<script>
+import apiBoard from '@/api/board';
+
+export default {
+  mounted() {
+    apiBoard.getArticle(1)
+      .then(function (response) {
+        console.log("getArticle", response);
+      })
+      .catch(function (e) {
+        console.log(e);
+      });
+  }
+}
+</script>
